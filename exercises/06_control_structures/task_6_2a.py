f68a194dfc5dcb49bc611791defa58957056c2f1
Ivan <twist9743@mail.ru>
2020-09-22 20:47:57 +0300

    third

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -18,13 +18,24 @@
 
 """
 ip = input('Введите IP - адрес: ')
-ip = ip.replace('.', ' ')
-ip = ip.split()
 bad = 'Неправильный IP-адрес'
-if len(ip) != 4:
+if ip.count('.') != 3:
     print(bad)
-for item in ip:
-    if not 0 <= int(ip) <= 255:
-        return bad
-
-print(ip)
+else:
+    ip = ip.split('.')
+    for i in ip:
+        try:
+            if int(i) < 0 or int(i) > 255:
+                print(bad)
+        except BaseException:
+            print(bad)
+    if int(ip[0]) > 0 and int(ip[0]) < 224:
+        print('unicast')
+    elif int(ip[0]) > 223 and int(ip[0]) < 240:
+        print('multicast')
+    elif int(ip[0]) == 255 and int(ip[1]) == 255 and int(ip[2]) == 255 and int(ip[3]) == 255:
+        print('local broadcast')
+    elif int(ip[0]) == 0 and int(ip[1]) == 0 and int(ip[2]) == 0 and int(ip[3]) == 0:
+        print('unassigned')
+    else:
+        print('unused')
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -9,3 +9,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите IP - адрес: ')
+bad = 'Неправильный IP-адрес'
+ip_correct = False
+while not ip_correct:
+    if ip.count('.') != 3:
+        print(bad)
+        ip = input('Введите IP - адрес еще раз: ')
+    else:
+        ip = ip.split('.')
+        for i in ip:
+            try:
+                if int(i) < 0 or int(i) > 255:
+                    print(bad)
+                    ip = input('Введите IP - адрес еще раз: ')
+            except BaseException:
+                print(bad)
+                ip = input('Введите IP - адрес еще раз: ')
+        ip_correct = True
+        if int(ip[0]) > 0 and int(ip[0]) < 224:
+            print('unicast')
+        elif int(ip[0]) > 223 and int(ip[0]) < 240:
+            print('multicast')
+        elif int(ip[0]) == 255 and int(ip[1]) == 255 and int(ip[2]) == 255 and int(ip[3]) == 255:
+            print('local broadcast')
+        elif int(ip[0]) == 0 and int(ip[1]) == 0 and int(ip[2]) == 0 and int(ip[3]) == 0:
+            print('unassigned')
+        else:
+            print('unused')
