da14ca2191fab237a4146ef72472788abce69939
Ivan <twist9743@mail.ru>
2021-01-07 20:23:10 +0300

    lab5

--- lab3 — копия/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY= b'\xeal\xcf]\xfftx\xec\xadb\xd7\xa85\xd4i\x82'
-MYSQL_USER = 'std_1085'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_1085'
-MYSQL_PASSWORD = 'Ukflbfnjh@123459'
